8732962baef3dcac3de2c57ff9d05a6ffeec50e0
ziddane11-oss <ziddane11@gmail.com>
2026-01-13 08:14:09 +0900

    Add useErrorWebSocket hook with mock implementation

---
+++ client/src/hooks/useErrorWebSocket.tsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from 'react';
+
+export const useErrorWebSocket = () => {
+  // 빌드 에러 방지용 임시 코드
+  console.log("Mock useErrorWebSocket");
+  return [];
+};
+
+export default useErrorWebSocket;
